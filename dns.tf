f84414db6c5af4c16f5f1450b389e55d7bf81667
Stuart Harland <stuartharland@Stuarts-MacBook-Pro.local>
2020-01-21 17:54:58 +0000

    instance naming

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
 resource "dns_a_record_set" "bauhaus" {
   zone = "${var.client_name}.${data.terraform_remote_state.dns.outputs.domain}."
-  name = "bauhaus-${var.environment}-${var.instance}"
+  name = "bauhaus-${var.instance}.${var.environment}"
 
   addresses = [
     google_compute_global_address.bauhaus.address
--- ssl.tf
+++ ssl.tf
@@ -1,9 +1,9 @@
 resource google_compute_managed_ssl_certificate bauhaus {
   provider = google-beta
 
-  name = "bauhaus-${var.environment}-${var.client_name}-${var.instance}"
+  name = "bauhaus-${var.environment}-${var.client_name}-${var.namespace}-${var.instance}"
 
   managed {
-    domains = ["bauhaus-${var.environment}.${var.client_name}-${var.instance}.${data.terraform_remote_state.dns.outputs.domain}."]
+    domains = ["bauhaus-${var.instance}.${var.environment}.${var.client_name}.${data.terraform_remote_state.dns.outputs.domain}."]
   }
 }
